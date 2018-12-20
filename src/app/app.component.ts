2e1acfbfc146b01e1c9489d7c2dd1a0c70690d6a
jordiascension <jordiascension@enterpriseconsulting.es>
2018-12-20 11:31:07 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private count = 1;
+
+  phoneNumberIds: number[] = [1];
+
+  @ViewChild('myForm')
+  private myForm: NgForm;
+
+  constructor() {
+  }
+
+  remove(i: number) {
+    this.phoneNumberIds.splice(i, 1);
+  }
+
+  add() {
+    this.phoneNumberIds.push(++this.count);
+  }
+
+
+  printMyForm() {
+    console.log(this.myForm);
+  }
+
+  register(myForm: NgForm) {
+    console.log('Registration successful.');
+    console.log(myForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { ShowErrorsComponent } from '../show-errors-component';
+
+import { BirthYearValidatorDirective } from '../validators/birth-year-validator.directive';
+import { TelephoneNumberFormatValidatorDirective } from '../validators/telephone-number-format-validator.directive';
+import { CountryCityValidatorDirective } from '../validators/country-city-validator.directive';
+import { TelephoneNumbersValidatorDirective } from '../validators/telephone-numbers-validator.directive';
+import { UniqueNameValidatorDirective } from '../validators/name-unique-validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ShowErrorsComponent,
+    BirthYearValidatorDirective,
+    TelephoneNumberFormatValidatorDirective,
+    CountryCityValidatorDirective,
+    TelephoneNumbersValidatorDirective,
+    UniqueNameValidatorDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
